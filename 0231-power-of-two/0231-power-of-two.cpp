da7bdef2ed64273a10f127fb60575c2576362ead
Kotini Sai Priya <ksaipriyakotini@gmail.com>
2026-08-09 14:00:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.71%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int count = 0;
+        while (n>0){
+            int last = n & 1;
+
+            n = n >> 1;
+            if (last == 1){
+                count++;
+
+            } 
+        }
+
+        if (count == 1){
+            return true ;
+        }else{
+            return false;
+        }    
+        }
+    
+};
